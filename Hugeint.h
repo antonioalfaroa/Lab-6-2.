1772e19567d07c3879fe2b8f8cc76db12ec87337
Antonio <78964462+antonioalfaroa@users.noreply.github.com>
2022-06-30 06:56:35 +0000

    Initial commit

---
+++ Hugeint.h
@@ -0,0 +1,38 @@
+// Hugeint.h
+// HugeInt class definition.
+#ifndef HUGEINT_H
+#define HUGEINT_H
+
+#include <array>
+#include <iostream>
+#include <string>
+
+class HugeInt{
+  friend std::ostream &operator<<( std::ostream &, const HugeInt & );
+  public:
+    static const int digits = 30; // maximum digits in a HugeInt
+
+    HugeInt( long = 0 ); // conversion/default constructor
+    HugeInt( const std::string & ); // conversion constructor
+
+  // addition operator; HugeInt + HugeInt
+    HugeInt operator+( const HugeInt & ) const;
+
+  // addition operator; HugeInt + int
+    HugeInt operator+( int ) const;
+
+  // addition operator;
+  // HugeInt + string that represents large integer value
+    HugeInt operator+( const std::string & ) const;
+
+    HugeInt operator*(const HugeInt &) const;
+
+	  HugeInt operator/(const HugeInt &) const;
+
+	  HugeInt operator=(const HugeInt &);
+  private:
+    std::array< short, digits > integer;
+
+}; // end class HugetInt
+
+#endif
